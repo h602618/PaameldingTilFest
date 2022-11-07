08dbd2af824763c30efb09b5f4c1d82de76600c3
h602618 <h602618@stud.hvl.no>
2022-11-07 09:31:34 +0100

    Move password to hash & salt tables
    * Insert data no longer valid

--- src/main/sql/user.sql
+++ src/main/sql/user.sql
@@ -1,14 +1,21 @@
+-- Drop existing table --
 DROP TABLE "user" CASCADE;
 
+-- Create new table --
 CREATE TABLE "user"
 (
     phone      INTEGER PRIMARY KEY,
     first_name VARCHAR(20)                                             NOT NULL,
     last_name  VARCHAR(20)                                             NOT NULL,
     gender     VARCHAR(6) check (gender = 'male' or gender = 'female') NOT NULL,
-    password   VARCHAR(255)                                            NOT NULL --TODO limit in java & javascript/html
+    hash       VARCHAR(64)                                             NOT NULL,
+    salt       VARCHAR(62)                                             NOT NULL
 );
 
+--salt=32
+--hash=64
+
+-- Insert data --
 INSERT INTO "user"
 VALUES (23456789, 'Anne', 'Panne', 'female', 'pass'),
        (90123456, 'Arne', 'Arnesen', 'male', 'pass'),
@@ -17,5 +24,7 @@ VALUES (23456789, 'Anne', 'Panne', 'female', 'pass'),
        (34534534, 'Per', 'Viskelær', 'male', 'pass'),
        (12345676, 'Pål', 'Olsen', 'male', 'pass'),
        (12321378, 'Xx-x', 'Xxx', 'female', 'pass');
+
+-- List all users --
 SELECT *
 FROM "user";
